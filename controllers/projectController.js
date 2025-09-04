d1e7064a55d3142a3ec59aacb463d9ef2288e042
James Lee <aardvarkpepper@gmail.com>
2025-09-03 20:48:17 -0400

    Add createTask and getAllTasksByProject.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Project = require('../../models/Project');
+const Task = require('../../models/Task');
 
 // split out child router later
 
@@ -44,12 +45,12 @@ const createProject = async (req, res) => {
     if (!req.user._id) {
       throw new Error("No user._id detected.  The user may not be logged in.")
     }
-    const project = await Project.create({
+    const createdProject = await Project.create({
       ...req.body,
       user: req.user._id,
       // The user ID needs to be added here - done.
     });
-    res.status(201).json(project);
+    res.status(201).json(createdProject);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -98,21 +99,69 @@ const deleteProjectById = async (req, res) => {
   }
 }; // deleteProjectById
 
+// Below will require { mergeParams: true } when split into child router, invoked in router (?)
+const createTask = async (req, res) => {
+  try {
+    if (!req.user._id) {
+      throw new Error("No user._id detected.  The user may not be logged in.")
+    }
+    // replicating getProjectById functionality; later figure out passing reference.
+    const projectRetrieved = await Project.findOne({ _id: req.params.projectId }); // why didn't I use findById?  Practice, why not.
+    // could just pop another field in the findOne argument, but eh.
+    if (!projectRetrieved) {
+      return res.status(404).json({ message: 'No project found with this id!' });
+    }
+    if (projectRetrieved.user !== req.user._id) {
+      return res.status(403).json({ messsage: '403 Forbidden; user is not authorized to add a task to this project as project does not belong to this user.' });
+    }
+
+    // By now, we know the user is logged in, the project exists, and the project belongs to the user.  Execution halts either by throwing Error or returning res if any of the previous is true; the execution by err stops as it's the last command in the function (also explicit return added).
+
+    // So we can now create a task belonging to the project belonging to the user.
+    const createdTask = await Task.create({
+      ...req.body,
+      project: req.params.projectId,
+      // The user ID needs to be added here - done.
+    });
+    return res.status(201).json(createdTask); // note that projectId must be added to sampleData for sample reference!
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+}; // createTask
+
+const getAllTasksByProject = async (req, res) => {
+  try {
+    if (!req.user._id) {
+      throw new Error("No user._id detected.  The user may not be logged in.")
+    }
+    // replicating getProjectById functionality; later figure out passing reference.
+    const projectRetrieved = await Project.findById(req.params.projectId);
+    if (!projectRetrieved) {
+      return res.status(404).json({ message: `No project found with ID:${req.params.ProjectId}!` });
+    }
+    if (projectRetrieved.user !== req.user._id) {
+      return res.status(403).json({ messsage: `403 Forbidden; user is not authorized to access all tasks of ${req.params.ProjectId} as project does not belong to this user.` });
+    }
+
+    // By now, we know the user is logged in, the project exists, and the project belongs to the user.  Execution halts either by throwing Error or returning res if any of the previous is true; the execution by err stops as it's the last command in the function (also explicit return added).
+
+    // So we can now create a task belonging to the project belonging to the user.
+    const tasks = await Task.find({ project: req.params.ProjectId });
+    return res.json({tasks: tasks, message: `All tasks for project:${req.params.ProjectId}`});
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}; //getAllProjects
+
 module.exports = {
-  createProject, 
-  getAllProjects, 
-  getProjectById, 
-  updateProjectById, 
+  createProject,
+  getAllProjects,
+  getProjectById,
+  updateProjectById,
   deleteProjectById,
   createTask,
-  getTasksByProject
+  getAllTasksByProject
 };
-
-// router.post('/', verifyAuthentication. createProject);
-// router.get('/', verifyAuthentication, getAllProjects);
-// router.get('/:id', verifyAuthentication, getProjectById);
-// router.put('/:id', verifyAuthentication, updateProjectById);
-// router.delete('/:id', verifyAuthentication, deleteProjectById);
 
 // // split below into child router later
 // router.post('/:projectId/tasks', verifyAuthentication, createTask); // see what happens when put last.  after all / routes okay.  Also see what happens if importing.
